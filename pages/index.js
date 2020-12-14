8f19e946e2e9cf68244d55f27b71d638fe6416d4
vallimmateus <75454379+vallimmateus@users.noreply.github.com>
2020-12-14 18:41:51 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+            <h1>Home</h1>
+            <p>Teste para o corno acreditar no Vallim</p>
+        </>
+    );
 }
